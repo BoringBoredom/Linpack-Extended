82b1531b0709a346e3956c3f43e6ebf4c18f2ca1
BoringBoredom <BoringBoredom>
2021-08-27 17:30:37 +0200

    improve resource usage

--- dependencies/linpack.js
+++ dependencies/linpack.js
@@ -2,44 +2,25 @@ const {spawn} = require('child_process')
 const {readFileSync, writeFileSync} = require('fs')
 const {join} = require('path')
 
-let timeToRun, problemSize, output, residual
-let total = 0, avg = 0, previousTrials = 0, trials = 0, min = 9999, max = 0
-let isRun = false
-const problemSizes = []
-
-function handleError(error) {
-    console.error(error)
-    process.exit()
-}
-
-function readConfig() {
-    try {
-        const lines = readFileSync(join(__dirname, '..', 'config.ini'), 'utf8').split('\n')
-        timeToRun = parseInt(lines[4]) * 60000
-        output = lines[10].toLowerCase() === 'true'
-        for (const problemSize of lines[7].split(' ')) {
-            problemSizes.push(problemSize)
-        }
-    }
-    catch (error) {
-        handleError(error)
-    }
-}
-
-function printStats() {
-    console.log(`Problem size: ${problemSize} | Trials completed: ${trials} | GFlops - Min: ${min} Avg: ${avg.toFixed(4)} Max: ${max}`)
-}
-
-function startRun() {
+function startRun(problemSize, timeToRun, output) {
     return new Promise((resolve) => {
-        try {
-            writeFileSync('config', `\n\n1\n${problemSize}\n${problemSize}\n99999\n4`)
-        }
-        catch (error) {
-            handleError(error)
-        }
-        isRun = false
+        writeFileSync('config', `\n\n1\n${problemSize}\n${problemSize}\n99999\n4`)
+        let isRun = false
+        let residual
+        let total = 0, avg = 0, previousTrials = 0, trials = 0, min = 9999, max = 0
         const startTime = Date.now()
+        const timer = setInterval(() => {
+            if (trials > previousTrials) {
+                console.log(`Problem size: ${problemSize} | Trials completed: ${trials} | GFlops - Min: ${min} Avg: ${avg.toFixed(4)} Max: ${max}`)
+                previousTrials = trials
+            }
+            if ((Date.now() - startTime) >= timeToRun) {
+                clearInterval(timer)
+                linpack.kill()
+                console.log(`Passed ${problemSize} problem size`)
+                resolve()
+            }
+        }, 10000)
         const linpack = spawn('linpack_xeon64.exe', ['config'])
         linpack.stdout.on('data', (data) => {
             const line = data.toString()
@@ -55,54 +36,38 @@ function startRun() {
                     trials++
                 }
             }
-            else {
-                if (element.length > 2) {
-                    if (element[5] !== residual || element[7] !== 'pass') {
-                        console.log('FAIL OR RESIDUAL MISMATCH')
-                        linpack.kill()
-                        process.exit()
-                    }
-                    const gflops = parseFloat(element[4])
-                    if (gflops < min) {
-                        min = gflops
-                    }
-                    if (gflops > max) {
-                        max = gflops
-                    }
-                    total += gflops
-                    trials++
-                    avg = total / trials
-                }
-                if ((Date.now() - startTime) >= timeToRun) {
-                    printStats()
-                    console.log(`Passed ${problemSize} problem size`)
+            else if (element.length > 2) {
+                if (element[5] !== residual || element[7] !== 'pass') {
                     linpack.kill()
-                    min = 9999
-                    max = avg = trials = previousTrials = total = 0
-                    resolve()
+                    console.log('FAIL OR RESIDUAL MISMATCH')
+                    process.exit()
                 }
+                const gflops = parseFloat(element[4])
+                if (gflops < min) {
+                    min = gflops
+                }
+                if (gflops > max) {
+                    max = gflops
+                }
+                total += gflops
+                trials++
+                avg = total / trials
             }
         })
         linpack.stderr.on('data', (data) => {
             linpack.kill()
-            handleError(data.toString())
+            console.error(data.toString())
+            process.exit()
         })
     })
 }
 
 async function main() {
-    readConfig()
-    setInterval(() => {
-        if (trials > previousTrials) {
-            printStats()
-            previousTrials = trials
-        }
-    }, 15000)
+    const lines = readFileSync(join(__dirname, '..', 'config.ini'), 'utf8').split('\n')
     console.log('Linpack Extended\nhttps://github.com/BoringBoredom/Linpack-Extended\nTest started')
-    for (const size of problemSizes) {
-        problemSize = size
-        console.log(`Current problem size: ${problemSize}`)
-        await startRun()
+    for (const size of lines[7].split(' ')) {
+        console.log(`Current problem size: ${size}`)
+        await startRun(size, parseInt(lines[4]) * 60000, lines[10].toLowerCase() === 'true')
     }
     console.log('Test finished')
     process.exit()
