1480a1cff803d2e62799caed9bee3c2563128b33
BoringBoredom <BoringBoredom>
2021-08-28 04:02:38 +0200

    improve notifications

--- dependencies/linpack.js
+++ dependencies/linpack.js
@@ -2,26 +2,25 @@ const {spawn} = require('child_process')
 const {readFileSync, writeFileSync} = require('fs')
 const {join} = require('path')
 
-function startRun(problemSize, leadingDimension, alignmentValue, libraryVersion, timeToRun, reducedOutput) {
+function startRun(problemSize, leadingDimension, alignmentValue, libraryVersion, timeToRun, reducedOutput, residualCheck) {
     return new Promise((resolve) => {
         writeFileSync('config', `\n\n1\n${problemSize}\n${leadingDimension}\n99999\n${alignmentValue}`)
         let isRun = false
         let residual
         let total = 0, avg = 0, previousTrials = 0, trials = 0, min = 9999, max = 0
-        console.log(`Current problem size: ${problemSize}`)
+        console.log(`\nTime to run: ${timeToRun / 60000} minutes\nProblem size: ${problemSize}\nLeading Dimension: ${leadingDimension}\nAlignment Value: ${alignmentValue}\nLibrary version: ${libraryVersion}\n`)
         const startTime = Date.now()
         const timer = setInterval(() => {
             if (trials > previousTrials) {
-                console.log(`Problem size: ${problemSize} | Trials completed: ${trials} | GFlops - Min: ${min} Avg: ${avg.toFixed(4)} Max: ${max}`)
+                console.log(`Trials completed: ${trials} | GFlops - Min: ${min} Avg: ${avg.toFixed(4)} Max: ${max}`)
                 previousTrials = trials
                 if ((Date.now() - startTime) >= timeToRun) {
-                    console.log(`Passed ${problemSize} problem size`)
                     clearInterval(timer)
                     linpack.kill()
                     resolve()
                 }
             }
-        }, 10000)
+        }, 20000)
         const linpack = spawn(join(__dirname, libraryVersion, 'linpack_xeon64.exe'), ['config'])
         linpack.stdout.on('data', (data) => {
             const line = data.toString()
@@ -38,10 +37,17 @@ function startRun(problemSize, leadingDimension, alignmentValue, libraryVersion,
                 }
             }
             else if (element.length > 2) {
-                if (element[5] !== residual || element[7] !== 'pass') {
-                    console.log('FAIL OR RESIDUAL MISMATCH')
+                if (element[7] !== 'pass') {
+                    console.log('FAIL')
                     linpack.kill()
                     process.exit()
+                }
+                if (residualCheck) {
+                    if (element[5] !== residual) {
+                        console.log('RESIDUAL MISMATCH')
+                        linpack.kill()
+                        process.exit()
+                    }
                 }
                 const gflops = parseFloat(element[4])
                 if (gflops < min) {
@@ -65,11 +71,11 @@ function startRun(problemSize, leadingDimension, alignmentValue, libraryVersion,
 
 async function main() {
     const config = JSON.parse(readFileSync(join(__dirname, '..', 'config.json')))
-    console.log('Linpack Extended\nhttps://github.com/BoringBoredom/Linpack-Extended\nTest started')
+    console.log('Linpack Extended\nhttps://github.com/BoringBoredom/Linpack-Extended')
     for (const test of config.tests) {
-        await startRun(test['problem size'].toString(), test['leading dimension'], test['alignment value'], test['library version'], test['minutes'] * 60000, config.settings['reduce terminal output'])
+        await startRun(test['problem size'].toString(), test['leading dimension'], test['alignment value'], test['library version'], test['minutes'] * 60000, config.settings['reduce terminal output'], config.settings['stop after residual mismatch'])
     }
-    console.log('Test finished')
+    console.log('All tests finished')
 }
 
 main()
