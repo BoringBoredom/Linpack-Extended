0ec6fd3cd9593315373d7cb358f5c87fe8117621
BoringBoredom <BoringBoredom>
2022-08-23 17:45:57 +0300

    refactor

--- dependencies/linpack.js
+++ dependencies/linpack.js
@@ -1,133 +1,161 @@
-const { spawn } = require('child_process')
-const { writeFileSync } = require('fs')
-const { join } = require('path')
+const { spawn } = require("child_process");
+const { writeFileSync } = require("fs");
+const { join } = require("path");
 
-function startRun(problemSize, leadingDimension, alignmentValue, timeToRun, reducedOutput, statTracker, residualCheck) {
-    return new Promise((resolve) => {
-        writeFileSync('temp', `\n\n1\n${problemSize}\n${leadingDimension}\n99999\n${alignmentValue}`)
+function startRun(
+   problemSize,
+   leadingDimension,
+   alignmentValue,
+   timeToRun,
+   reducedOutput,
+   statTracker,
+   residualCheck
+) {
+   return new Promise((resolve) => {
+      writeFileSync(
+         "temp",
+         `\n\n1\n${problemSize}\n${leadingDimension}\n99999\n${alignmentValue}`
+      );
 
-        let isRun = false
-        let residual, min, avg, max, total
-        let trials = 0, previousTrials = 0
+      let isRun = false;
+      let residual, min, avg, max, total;
+      let trials = 0;
+      let previousTrials = 0;
 
-        console.log(
-            '\n' +
+      console.log(
+         "\n" +
             `Start time: ${new Date().toLocaleTimeString([], {
-                hour12: false,
-                hour: '2-digit',
-                minute: '2-digit'
+               hour12: false,
+               hour: "2-digit",
+               minute: "2-digit"
             })}\n` +
             `Time to run: ${timeToRun / 60000} minutes\n` +
             `Problem size: ${problemSize}\n` +
             `Leading dimension: ${leadingDimension}\n` +
             `Alignment value: ${alignmentValue}\n` +
-            `Residual checks: ${residualCheck ? 'ON' : 'OFF'}\n`
-        )
+            `Residual checks: ${residualCheck ? "ON" : "OFF"}\n`
+      );
 
-        const startTime = Date.now()
+      const startTime = Date.now();
 
-        const timer = setInterval(async () => {
-            if (trials > previousTrials) {
-                if (statTracker) {
-                    console.log(`Trials completed: ${trials.toString().padStart(4, '0')} | GFlops - Min: ${min.toFixed(4)} Avg: ${avg.toFixed(4)} Max: ${max.toFixed(4)}`)
-                }
-                previousTrials = trials
-                if ((Date.now() - startTime) >= timeToRun) {
-                    clearInterval(timer)
-                    linpack.kill()
-                    await new Promise(r => setTimeout(r, 5000))
-                    resolve()
-                }
+      const timer = setInterval(async () => {
+         if (trials > previousTrials) {
+            if (statTracker) {
+               console.log(
+                  `Trials completed: ${trials
+                     .toString()
+                     .padStart(4, "0")} | GFlops - Min: ${min.toFixed(
+                     4
+                  )} Avg: ${avg.toFixed(4)} Max: ${max.toFixed(4)}`
+               );
             }
-        }, 20000)
 
-        const linpack = spawn(
-            'linpack_xeon64.exe', [join(__dirname, 'temp')], {
-                cwd: join(__dirname, 'linpack'),
-                env: { KMP_AFFINITY: 'nowarnings,compact,1,0,granularity=fine' }
+            previousTrials = trials;
+
+            if (Date.now() - startTime >= timeToRun) {
+               clearInterval(timer);
+               linpack.kill();
+               await new Promise((r) => setTimeout(r, 5000));
+               resolve();
             }
-        )
-        linpack.stdout.on('data', (data) => {
-            const line = data.toString()
-            if (!reducedOutput) {
-                process.stdout.write(line)
+         }
+      }, 20000);
+
+      const linpack = spawn("linpack_xeon64.exe", [join(__dirname, "temp")], {
+         cwd: join(__dirname, "linpack"),
+         env: { KMP_AFFINITY: "nowarnings,compact,1,0,granularity=fine" }
+      });
+
+      linpack.stdout.on("data", (data) => {
+         const line = data.toString();
+
+         if (!reducedOutput) {
+            process.stdout.write(line);
+         }
+
+         const element = line.split(/\s+/);
+
+         if (!isRun) {
+            if (element[0] === problemSize && element[1] === problemSize) {
+               isRun = true;
+               residual = element[5];
+               trials++;
+               if (statTracker) {
+                  min = max = avg = total = parseFloat(element[4]);
+               }
             }
-            const element = line.split(/\s+/)
-            if (!isRun) {
-                if (element[0] === problemSize && element[1] === problemSize) {
-                    isRun = true
-                    residual = element[5]
-                    trials++
-                    if (statTracker) {
-                        min = max = avg = total = parseFloat(element[4])
-                    }
-                }
+         } else if (element.length > 2) {
+            if (element[7] !== "pass") {
+               console.log("\n\n" + "FAIL - severe instability detected\n\n");
+               linpack.kill();
+               process.exit();
             }
-            else if (element.length > 2) {
-                if (element[7] !== 'pass') {
-                    console.log(
-                        '\n\n' +
-                        'FAIL - severe instability detected\n\n'
-                    )
-                    linpack.kill()
-                    process.exit()
-                }
-                if (residualCheck && element[5] !== residual) {
-                    console.log(
-                        '\n\n' +
-                        'RESIDUAL MISMATCH - instability detected (residual checks can be turned off in the config)\n\n'
-                    )
-                    linpack.kill()
-                    process.exit()
-                }
-                trials++
-                if (statTracker) {
-                    const gflops = parseFloat(element[4])
-                    if (gflops < min) {
-                        min = gflops
-                    }
-                    if (gflops > max) {
-                        max = gflops
-                    }
-                    total += gflops
-                    avg = total / trials
-                }
+
+            if (residualCheck && element[5] !== residual) {
+               console.log(
+                  "\n\n" +
+                     "RESIDUAL MISMATCH - instability detected (residual checks can be turned off in the config)\n\n"
+               );
+               linpack.kill();
+               process.exit();
             }
-        })
-        linpack.stderr.on('data', (data) => {
-            console.error(data.toString())
-            linpack.kill()
-            process.exit()
-        })
-    })
+
+            trials++;
+
+            if (statTracker) {
+               const gflops = parseFloat(element[4]);
+
+               if (gflops < min) {
+                  min = gflops;
+               }
+
+               if (gflops > max) {
+                  max = gflops;
+               }
+
+               total += gflops;
+               avg = total / trials;
+            }
+         }
+      });
+
+      linpack.stderr.on("data", (data) => {
+         console.error(data.toString());
+         linpack.kill();
+         process.exit();
+      });
+   });
 }
 
 async function main() {
-    const config = require(join(__dirname, '..', 'config.json'))
+   const config = require(join(__dirname, "..", "config.json"));
 
-    console.log(
-        'Linpack Extended\n' +
-        'https://github.com/BoringBoredom/Linpack-Extended'
-    )
+   console.log(
+      "Linpack Extended\n" + "https://github.com/BoringBoredom/Linpack-Extended"
+   );
 
-    for (const test of config['test order']) {
-        const currentTest = config.tests[test.toString()]
-        await startRun(
-            currentTest['problem size'].toString(),
-            currentTest['leading dimension'],
-            currentTest['alignment value'],
-            currentTest['minutes'] * 60000,
-            currentTest['problem size'] < config.settings['reduce output below X problem size'],
-            currentTest['problem size'] < config.settings['track stats below X problem size'],
-            config.settings['stop after residual mismatch']
-        )
-    }
+   for (const test of config["test order"]) {
+      const currentTest = config.tests[test.toString()];
+      await startRun(
+         currentTest["problem size"].toString(),
+         currentTest["leading dimension"],
+         currentTest["alignment value"],
+         currentTest["minutes"] * 60000,
+         currentTest["problem size"] <
+            config.settings["reduce output below X problem size"],
+         currentTest["problem size"] <
+            config.settings["track stats below X problem size"],
+         config.settings["stop after residual mismatch"]
+      );
+   }
 
-    console.log(
-        '\n\n' +
-        'All tests successfully passed\n' +
-        `Residual checks are ${config.settings['stop after residual mismatch'] ? 'ON' : 'OFF'}\n\n`
-    )
+   console.log(
+      "\n\n" +
+         "All tests successfully passed\n" +
+         `Residual checks are ${
+            config.settings["stop after residual mismatch"] ? "ON" : "OFF"
+         }\n\n`
+   );
 }
 
-main()
+main();
--- dependencies/updateCheck.js
+++ dependencies/updateCheck.js
@@ -1,32 +1,41 @@
-const {get} = require('https')
-const {readFileSync} = require('fs')
-const {createInterface} = require('readline')
+const { get } = require("https");
+const { readFileSync } = require("fs");
+const { createInterface } = require("readline");
 
 get(process.argv[2], (res) => {
-    if (Math.floor(res.statusCode / 400) === 1) {
-        res.resume()
-        return
-    }
-    res.setEncoding('utf8')
-    let data = ''
-    res.on('data', (chunk) => {
-        data += chunk
-    })
-    res.on('end', () => {
-        const latestVersion = data.split('.')
-        const currentVersion = readFileSync('version', 'utf8').split('.')
-        for (let index = 0; index < 3; index++) {
-            if (parseInt(latestVersion[index]) > parseInt(currentVersion[index])) {
-                const splitURL = process.argv[2].split('/')
-                const rl = createInterface({
-                    input: process.stdin,
-                    output: process.stdout
-                })
-                rl.question(`New version available @ https://github.com/${splitURL[3]}/${splitURL[4]}. Press ENTER to continue...`, () => {
-                    rl.close()
-                })
-                break
-            }
-        }
-    })
-}).on('error', () => {})
+   if (Math.floor(res.statusCode / 400) === 1) {
+      res.resume();
+      return;
+   }
+
+   res.setEncoding("utf8");
+   let data = "";
+
+   res.on("data", (chunk) => {
+      data += chunk;
+   });
+
+   res.on("end", () => {
+      const latestVersion = data.split(".");
+      const currentVersion = readFileSync("version", "utf8").split(".");
+
+      for (let index = 0; index < 3; index++) {
+         if (parseInt(latestVersion[index]) > parseInt(currentVersion[index])) {
+            const splitURL = process.argv[2].split("/");
+            const rl = createInterface({
+               input: process.stdin,
+               output: process.stdout
+            });
+
+            rl.question(
+               `New version available @ https://github.com/${splitURL[3]}/${splitURL[4]}. Press ENTER to continue...`,
+               () => {
+                  rl.close();
+               }
+            );
+
+            break;
+         }
+      }
+   });
+}).on("error", () => {});
